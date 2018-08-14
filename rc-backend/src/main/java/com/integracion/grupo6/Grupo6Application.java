176e893cd9f70edde30e12e22ad4e0c71cb1dc94
Maximiliano Gazquez <maximiliano.628@gmail.com>
2018-08-13 21:50:26 -0300

    Se agrega FTP

--- rc-backend/src/main/java/com/integracion/grupo6/Grupo6Application.java
+++ rc-backend/src/main/java/com/integracion/grupo6/Grupo6Application.java
@@ -1,12 +1,16 @@
 package com.integracion.grupo6;
 
+import com.integracion.grupo6.ftp.ReclamosFtpServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Grupo6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Grupo6Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Grupo6Application.class, args);
+        context.getBean(ReclamosFtpServer.class).listen();
     }
 }
---
+++ rc-backend/src/main/java/com/integracion/grupo6/ftp/ReclamosFtpServer.java
@@ -0,0 +1,71 @@
+package com.integracion.grupo6.ftp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.listener.Listener;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.apache.ftpserver.*;
+
+@Component
+public class ReclamosFtpServer {
+
+    protected final Log logger = LogFactory.getLog(this.getClass());
+
+    private static final int DEFAULT_PORT = 2221;
+    private static final String DEFAULT_USER = "admin";
+    private static final String DEFAULT_PASSWORD = "reclamos";
+
+
+    @Value("${ftpserver.port}")
+    private String ftpPort;
+
+    @Value("${ftpserver.user.admin.homedirectory}")
+    private String homeDir;
+
+    public void listen() {
+        try {
+            getFtpServer().start();
+            logger.info(String.format("Corriendo server FTP en localhost:%d, user: '%s', password: '%s', home-dir: '%s'", safeGetPort(), DEFAULT_USER, DEFAULT_PASSWORD, homeDir));
+        } catch (FtpException ex) {
+            logger.warn("Error corriendo servidor FTP", ex);
+        }
+    }
+
+    private FtpServer getFtpServer() throws FtpException {
+        FtpServerFactory serverFactory = new FtpServerFactory();
+
+        serverFactory.addListener("default", getListener());
+        serverFactory.setUserManager(getUserManager());
+        FtpServer server = serverFactory.createServer();
+        return server;
+    }
+
+    private Listener getListener() {
+        int port = safeGetPort();
+        ListenerFactory listenerFactory = new ListenerFactory();
+        listenerFactory.setPort(port);
+        return listenerFactory.createListener();
+    }
+
+    private UserManager getUserManager() throws FtpException {
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        UserManager userManager = userManagerFactory.createUserManager();
+        return userManager;
+    }
+
+    private int safeGetPort() {
+        try {
+            return Integer.parseInt(ftpPort);
+        } catch (Exception ex) {
+            logger.warn("Error leyendo puerto para FTP, se va a utilizar el default " + DEFAULT_PORT);
+            return DEFAULT_PORT;
+        }
+    }
+
+}
